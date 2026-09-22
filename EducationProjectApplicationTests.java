75d315676972c665f1a469a318ead3f1fd7960b3
Sanandhyanamala <sanandhyanamala91@gmail.com>
2026-09-22 10:34:01 +0530

    Add files via upload

---
+++ EducationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
